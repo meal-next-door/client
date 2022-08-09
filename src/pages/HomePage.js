c4355790b5a95a041ec277333cdefc632e56bc80
Stephanie-Tovar <stephanie.anne.tovar@gmail.com>
2022-08-09 13:15:02 +0200

    add optional chain to cooks and meals

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,8 @@ function HomePage(props) {
 
     const users = props.users;
     const meals = props.meals;
-    const cooks = [...users].filter(user => user.role === "cook").sort((a, b) => b.createdAt.localeCompare(a.createdAt)).slice(0, 1);
-    const recentMeals = [...meals].sort((a, b) => b.createdAt.localeCompare(a.createdAt)).slice(0, 1);
+    const cooks = [...users]?.filter(user => user.role === "cook").sort((a, b) => b.createdAt.localeCompare(a.createdAt)).slice(0, 1);
+    const recentMeals = [...meals]?.sort((a, b) => b.createdAt.localeCompare(a.createdAt)).slice(0, 1);
 
 
     return (
