177004846ba65d5a9bcce23f274e712f220354ff
Stephanie-Tovar <stephanie.anne.tovar@gmail.com>
2022-08-10 14:12:20 +0200

    remove repeated code after merge

--- src/pages/MealsList.js
+++ src/pages/MealsList.js
@@ -97,40 +97,6 @@ function MealsList(props) {
 
                 </Row>
             </Container>
-            <form>
-                <label>Search</label>
-                <input type="text" value={search} onChange={(e) => handleSearch(e.target.value)} />
-            </form>
-
-            <button onClick={() => handleDiet('vegetarian')}>Vegetarian</button>
-            <button onClick={() => handleDiet('vegan')}>Vegan</button>
-            <button onClick={() => handleDiet('gluten-free')}>Gluten-free</button>
-            <button onClick={() => handleDiet('dairy-free')}>Dairy-free</button>
-            <button onClick={() => handleDiet('allergens-free')}>Allergens-free</button>
-            <button onClick={() => handleDiet('sugar-free')}>Sugar-free</button>
-            <button onClick={() => handleDiet('kosher')}>Kosher</button>
-            <button onClick={() => handleDiet('halal')}>Halal</button>
-            <button onClick={() => refreshList()}>Refresh</button>
-
-
-            {props?.meals?.map((meal) => {
-                return (
-                    <div className="meals card" key={meal._id} >
-                        <h3>{meal.title}</h3>
-
-                        {meal.diet?.map(e => {
-                            return <span>{e} </span>
-                        })}
-
-                        <p>Cook: {meal.cook?.username}</p>
-                        <p>{meal.description}</p>
-
-                        <NavLink to={`/meals/${meal._id}`}>
-                            <button>View details</button>
-                        </NavLink>
-                    </div>
-                );
-            })}
 
         </div>
     );
