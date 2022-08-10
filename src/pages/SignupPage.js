6d86d38a77b5b177ea9d24d01984bcba7b11cb31
“Alexandre <coma.alexandre@gmail.com>
2022-08-10 15:27:25 +0200

    CSS on signup page

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -3,6 +3,9 @@ import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 
 function SignupPage() {
@@ -33,7 +36,10 @@ function SignupPage() {
 
     return (
         
+        <Container >
         <Form onSubmit={handleSubmit}>
+        <Row className="justify-content-md-center">
+        <Col xs={4} xl={6} align>
             <Form.Group className="mb-3" controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
                 <Form.Control type="text" placeholder="Choose a username" value={username}
@@ -42,13 +48,21 @@ function SignupPage() {
                     We'll never share your email with anyone else.
                 </Form.Text>
             </Form.Group>
+            </Col>
+            </Row>
 
+            <Row className="justify-content-md-center">
+            <Col xs={4} xl={6} align>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" value={password}
                     onChange={(e) => setPassword(e.target.value)} />
             </Form.Group>
+            </Col>
+            </Row>
 
+            <Row className="justify-content-md-center">
+            <Col xs={4} xl={6} align>
             <Form.Group className="mb-3" controlId="formBasicAddress">
                 <Form.Label>Address</Form.Label>
                 <Form.Control type="text" placeholder="Enter address" value={address}
@@ -57,23 +71,33 @@ function SignupPage() {
                     Enter your address so that customers can find your delicious meals
                 </Form.Text>
             </Form.Group>
+            </Col>
+            </Row>
 
+            <Row className="justify-content-md-center">
+            <Col xs={4} xl={6} align>
             <Form.Select aria-label="select role">
                 <option value="cook">Cook</option>
                 <option value="customer">Customer</option>
             </Form.Select>
+            </Col>
+            </Row>
 
+            <Row className="justify-content-md-center">
+            <Col xs={4} xl={6} align>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button variant="primary" type="submit">
                 Submit
             </Button>
+            </Col>
+            </Row>
 
             <p>Already have account?</p>
             <NavLink to={"/login"}> Login</NavLink>
         </Form>
-
+        </Container>
     );
 }
 
